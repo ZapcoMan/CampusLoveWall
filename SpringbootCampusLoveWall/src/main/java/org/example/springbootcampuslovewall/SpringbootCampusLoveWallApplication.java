4699278ffda5615c5c3a76d06fe903f34924f84e
codervibe <3217277135@qq.com>
2025-08-20 07:15:56 +0800

    feat: 初始化 Springboot Campus Love Wall 项目
    - 创建 Maven 项目配置文件 pom.xml
    - 添加 Spring Boot 应用程序主类 SpringbootCampusLoveWallApplication
    - 创建测试用例类 SpringbootCampusLoveWallApplicationTests
    - 设置应用名称为 SpringbootCampusLoveWall

---
+++ SpringbootCampusLoveWall/src/main/java/org/example/springbootcampuslovewall/SpringbootCampusLoveWallApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootcampuslovewall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCampusLoveWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCampusLoveWallApplication.class, args);
+    }
+
+}
---
+++ SpringbootCampusLoveWall/src/test/java/org/example/springbootcampuslovewall/SpringbootCampusLoveWallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootcampuslovewall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCampusLoveWallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
