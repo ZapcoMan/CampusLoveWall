1e67c8eb4cb3a7d6fa1212941dc43854c46f047d
codervibe <3217277135@qq.com>
2025-08-20 14:38:43 +0800

    refactor(WebController): 重构 WebController 类
    - 添加 lombok.extern.slf4j.Slf4j 注解以集成日志功能
    - 将 Controller 注解替换为 RestController 注解
    - 添加 RootMapping 注解以指定请求路径
    - 修改 sayHello 方法名称为 hello
    -优化 hello 方法的返回值类型和结构

--- SpringbootCampusLoveWall/src/main/java/org/example/springbootcampuslovewall/controller/WebController.java
+++ SpringbootCampusLoveWall/src/main/java/org/example/springbootcampuslovewall/controller/WebController.java
@@ -1,14 +1,19 @@
 package org.example.springbootcampuslovewall.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.springbootcampuslovewall.common.result.R;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("")
+@Slf4j
+@RestController
+@RequestMapping("/web")
 public class WebController {
-    @GetMapping("/sayHello")
-    public R<String> sayHello() {
-        return R.ok("hello");
+    @GetMapping("/hello")
+    public R<Object> hello() {
+        return R.ok().message("hello");
     }
 
 }
