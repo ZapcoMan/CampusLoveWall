d564bdd98668e1beb1221b877e1e7e396adfc749
codervibe <3217277135@qq.com>
2025-08-20 12:29:54 +0800

    feat(controller): 添加 sayHello 接口
    - 在 WebController 中新增 sayHello 方法
    - 使用 @GetMapping 注解映射 /sayHello 请求
    - 返回 R.ok("hello") 响应，表示成功返回 "hello"

--- SpringbootCampusLoveWall/src/main/java/org/example/springbootcampuslovewall/controller/WebController.java
+++ SpringbootCampusLoveWall/src/main/java/org/example/springbootcampuslovewall/controller/WebController.java
@@ -1,8 +1,14 @@
 package org.example.springbootcampuslovewall.controller;
 
+import org.example.springbootcampuslovewall.common.result.R;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("")
 public class WebController {
+    @GetMapping("/sayHello")
+    public R<String> sayHello() {
+        return R.ok("hello");
+    }
 
 }
