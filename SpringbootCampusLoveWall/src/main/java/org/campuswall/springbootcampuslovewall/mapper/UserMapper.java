9210b1d2c50005fca3db60460777206be5478967
codervibe <3217277135@qq.com>
2025-08-27 00:35:30 +0800

    feat(mapper): 添加 UserMapper接口
    - 新增 UserMapper 接口，继承自 BaseMapperPlus
    - 用于处理用户相关数据的持久化操作

---
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package org.campuswall.springbootcampuslovewall.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.campuswall.springbootcampuslovewall.common.core.mapper.BaseMapperPlus;
+import org.campuswall.springbootcampuslovewall.entity.User;
+
+@Mapper
+public interface UserMapper extends BaseMapperPlus<User, Integer> {
+}
