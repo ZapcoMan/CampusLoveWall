808f7fbfffed3a81beacb62cdeeeedf34de19ea5
codervibe <3217277135@qq.com>
2025-08-26 21:09:07 +0800

    refactor(common): 优化角色枚举类文档注释
    -为 RoleEnum 类添加类级注释，说明其用途
    -为 fromCode 方法添加方法级注释，描述其功能和参数- 优化现有代码的注释，提高代码可读性

--- SpringbootCampusLoveWall/src/main/java/org/example/springbootcampuslovewall/common/enums/RoleEnum.java
+++ SpringbootCampusLoveWall/src/main/java/org/example/springbootcampuslovewall/common/enums/RoleEnum.java
@@ -2,6 +2,11 @@ package org.example.springbootcampuslovewall.common.enums;
 
 import lombok.Getter;
 
+// ... existing code ...
+/**
+ * 角色枚举类
+ * 定义系统中的角色类型，包括角色代码和标签
+ */
 @Getter
 public enum RoleEnum {
     ADMIN("ADMIN", "管理员");
@@ -14,6 +19,12 @@ public enum RoleEnum {
         this.label = label;
     }
 
+    /**
+     * 根据角色代码获取对应的角色枚举值
+     * @param code 角色代码
+     * @return 对应的角色枚举值
+     * @throws IllegalArgumentException 当提供的角色代码无效时抛出此异常
+     */
     public static RoleEnum fromCode(String code) {
         for (RoleEnum value : RoleEnum.values()) {
             if (value.getCode().equalsIgnoreCase(code)) {
