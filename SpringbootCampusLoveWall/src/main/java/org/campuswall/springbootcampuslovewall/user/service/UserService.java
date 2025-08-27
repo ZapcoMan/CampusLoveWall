0ca79f8b773c2311a2f312e23d829a4623fabd44
codervibe <3217277135@qq.com>
2025-08-28 00:47:21 +0800

    refactor(user): 重构用户模块代码结构
    - 将 UserService接口从 service 包移动到 user/service 包
    - 更新包名以更好地反映模块结构
    - 为 UserService 接口添加新的方法 login 和 updatePassword

---
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/user/service/UserService.java
@@ -0,0 +1,31 @@
+package org.campuswall.springbootcampuslovewall.user.service;
+
+import org.campuswall.springbootcampuslovewall.common.core.service.BaseService;
+import org.campuswall.springbootcampuslovewall.entity.Account;
+import org.campuswall.springbootcampuslovewall.user.entity.User;
+
+
+public interface UserService extends BaseService<User, Long> {
+    /**
+     * 根据用户名查找用户信息
+     *
+     * @param username 用户名
+     * @return 返回匹配的用户对象，如果未找到则返回null
+     */
+    User findByUsername(String username);
+
+    /**
+     * 用户登录
+     *
+     * @param account 账户信息
+     * @return 登录成功的账户信息
+     */
+    Account login(Account account);
+
+    /**
+     * 更新用户密码
+     *
+     * @param account 账户信息
+     */
+    void updatePassword(Account account);
+}
