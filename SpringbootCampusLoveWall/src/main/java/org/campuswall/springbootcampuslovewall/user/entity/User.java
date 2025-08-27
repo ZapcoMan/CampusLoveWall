df5eaa2c41c232e9862e2a4407d2c6f259c5bf91
codervibe <3217277135@qq.com>
2025-08-28 00:49:06 +0800

    refactor(user): 调整 User 实体类的包结构- 将 User 类从 entity包移动到 user.entity 包- 更新包名为 org.campuswall.springbootcampuslovewall.user.entity

--- SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/user/entity/User.java
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/user/entity/User.java
@@ -1,4 +1,4 @@
-package org.campuswall.springbootcampuslovewall.entity;
+package org.campuswall.springbootcampuslovewall.user.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
