09410b70647bed1e724a777232514e458e2a40eb
codervibe <3217277135@qq.com>
2025-08-31 06:10:02 +0800

    style(BaseServiceImpl): 优化代码格式和结构
    - 调整代码缩进和空格，提高可读性
    - 修正注释格式，保持一致性
    - 添加必要的换行，改善代码布局

--- SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/common/core/service/impl/BaseServiceImpl.java
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/common/core/service/impl/BaseServiceImpl.java
@@ -1,14 +1,17 @@
 package org.campuswall.springbootcampuslovewall.common.core.service.impl;
 
-import com.github.pagehelper.PageInfo;import org.campuswall.springbootcampuslovewall.admin.entity.Admin;import org.campuswall.springbootcampuslovewall.common.core.service.BaseService;
+import com.github.pagehelper.PageInfo;
+import org.campuswall.springbootcampuslovewall.admin.entity.Admin;
+import org.campuswall.springbootcampuslovewall.common.core.service.BaseService;
 
 import java.util.List;
 
 /**
  * 自定义通用 Service 实现类，使用传统 MyBatis Mapper 实现。
- * @param <T> 实体类
+ *
+ * @param <T>  实体类
  * @param <ID> 主键类型
- * @param <M> Mapper 类型
+ * @param <M>  Mapper 类型
  */
 public abstract class BaseServiceImpl<T, ID, M> implements BaseService<T, ID> {
 
@@ -23,7 +26,9 @@ public abstract class BaseServiceImpl<T, ID, M> implements BaseService<T, ID> {
     @Override
     public abstract T selectById(ID id);
 
-    public abstract void deleteById(Integer id);@Override
+    public abstract void deleteById(Integer id);
+
+    @Override
     public abstract List<T> selectAll();
 
     public abstract void updateById(Long id);
@@ -36,4 +41,6 @@ public abstract class BaseServiceImpl<T, ID, M> implements BaseService<T, ID> {
 
     @Override
     public abstract void deleteById(ID id);
-public abstract PageInfo<Admin> selectPage(Integer pageNum, Integer pageSize, Admin admin);}
+
+    public abstract PageInfo<Admin> selectPage(Integer pageNum, Integer pageSize, Admin admin);
+}
