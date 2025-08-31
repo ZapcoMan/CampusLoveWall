f6cc91a2ec4e0f35fa3738f7672bca7ffa366aca
codervibe <3217277135@qq.com>
2025-08-31 10:54:33 +0800

    refactor(BaseServiceImpl): 为 BaseServiceImpl 中的方法添加 Javadoc 注释
    - 为构造函数和所有抽象方法添加了详细的 Javadoc 注释
    - 注释包括方法的描述、参数说明和返回值说明
    - 优化了代码的可读性和文档性，便于其他开发者理解和使用

--- SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/common/core/service/impl/BaseServiceImpl.java
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/common/core/service/impl/BaseServiceImpl.java
@@ -17,30 +17,86 @@ public abstract class BaseServiceImpl<T, ID, M> implements BaseService<T, ID> {
 
     protected final M mapper;
 
+    /**
+     * 构造函数，初始化Mapper对象
+     *
+     * @param mapper 数据访问层Mapper对象
+     */
     public BaseServiceImpl(M mapper) {
         this.mapper = mapper;
     }
 
+    /**
+     * 根据主键ID查询实体对象
+     *
+     * @param id 主键ID（Long类型）
+     * @return 实体对象
+     */
     public abstract T selectById(Long id);
 
     @Override
+    /**
+     * 根据主键ID查询实体对象
+     *
+     * @param id 主键ID
+     * @return 实体对象
+     */
     public abstract T selectById(ID id);
 
+    /**
+     * 根据主键ID删除实体对象
+     *
+     * @param id 主键ID（Integer类型）
+     */
     public abstract void deleteById(Integer id);
 
     @Override
+    /**
+     * 查询所有实体对象列表
+     *
+     * @return 实体对象列表
+     */
     public abstract List<T> selectAll();
 
+    /**
+     * 根据主键ID更新实体对象
+     *
+     * @param id 主键ID（Long类型）
+     */
     public abstract void updateById(Long id);
 
     @Override
+    /**
+     * 插入实体对象
+     *
+     * @param entity 实体对象
+     */
     public abstract void insert(T entity);
 
     @Override
+    /**
+     * 根据主键ID更新实体对象
+     *
+     * @param entity 实体对象
+     * @return 是否更新成功
+     */
     public abstract boolean updateById(T entity);
 
     @Override
+    /**
+     * 根据主键ID删除实体对象
+     *
+     * @param id 主键ID
+     */
     public abstract void deleteById(ID id);
 
+    /**
+     * 分页查询管理员信息列表
+     *
+     * @param pageNum  页码
+     * @param pageSize 每页大小
+     * @param admin    查询条件对象
+     * @return 分页信息对象
+     */
     public abstract PageInfo<Admin> selectPage(Integer pageNum, Integer pageSize, Admin admin);
 }
