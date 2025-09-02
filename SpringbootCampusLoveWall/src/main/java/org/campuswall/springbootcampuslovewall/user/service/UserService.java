f69875e820dbb87e9f91e1000f137a17f468604b
codervibe <3217277135@qq.com>
2025-09-02 20:30:00 +0800

    refactor(user): 移除 UserService 接口中未使用的函数- 删除了未使用的 login 和 updatePassword 方法 - 移除了不必要的导入语句

--- SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/user/service/UserService.java
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/user/service/UserService.java
@@ -1,11 +1,7 @@
 package org.campuswall.springbootcampuslovewall.user.service;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import org.campuswall.springbootcampuslovewall.common.core.service.BaseService;
-import org.campuswall.springbootcampuslovewall.entity.Account;
 import org.campuswall.springbootcampuslovewall.user.entity.User;
-
-import java.util.List;
 
 
 public interface UserService extends BaseService<User, Long> {
@@ -17,20 +13,6 @@ public interface UserService extends BaseService<User, Long> {
      */
     User findByUsername(String username);
 
-    /**
-     * 用户登录
-     *
-     * @param account 账户信息
-     * @return 登录成功的账户信息
-     */
-    Account login(Account account);
-
-    /**
-     * 更新用户密码
-     *
-     * @param account 账户信息
-     */
-    void updatePassword(Account account);
 
 
 
