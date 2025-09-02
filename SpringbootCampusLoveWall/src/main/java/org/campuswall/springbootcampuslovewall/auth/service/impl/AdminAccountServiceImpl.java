5d70f539a1826394be474bd2a8e31487c35d76f4
codervibe <3217277135@qq.com>
2025-09-02 17:23:35 +0800

    feat(auth): 添加 AdminAccountService现了 AdminAccountService 接口接口的实现类
    - 实中的多个方法，包括登录、注册、密码重置等
    - 添加了分页查询管理员信息的方法- 构造函数中注入了 AdminMapper 对象

--- SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/auth/service/impl/AdminAccountServiceImpl.java
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/auth/service/impl/AdminAccountServiceImpl.java
@@ -31,5 +31,254 @@ public class AdminAccountServiceImpl extends BaseServiceImpl<Admin, Integer, Adm
     @Resource
     private AdminMapper adminMapper;
 
+    /**
+     * 构造函数，初始化Mapper对象
+     *
+     * @param adminMapper 数据访问层Mapper对象
+     */
+    public AdminAccountServiceImpl(AdminMapper adminMapper) {
+        super(adminMapper);
+    }
 
+
+    /**
+     * 根据主键ID查询实体对象
+     *
+     * @param id 主键ID（Long类型）
+     * @return 实体对象
+     */
+    @Override
+    public Admin selectById(Long id) {
+        return null;
+    }
+
+    /**
+     * 获取账户角色枚举
+     *
+     * @return RoleEnum 角色枚举值
+     */
+    @Override
+    public RoleEnum getRole() {
+        return null;
+    }
+
+    /**
+     * @param integer 账户ID
+     * @return
+     */
+    @Override
+    public Admin selectById(Integer integer) {
+        return null;
+    }
+
+    /**
+     * 账户登录
+     *
+     * @param account 账户信息
+     * @return T 登录成功的账户对象
+     */
+    @Override
+    public Admin login(Admin account) {
+        return null;
+    }
+
+    /**
+     * 更新账户密码
+     *
+     * @param account 包含密码信息的账户对象
+     */
+    @Override
+    public void updatePassword(Admin account) {
+
+    }
+
+    /**
+     * 注册新账户
+     *
+     * @param account 用户对象
+     */
+    @Override
+    public void register(Admin account) {
+
+    }
+
+    /**
+     * 重置账户密码
+     *
+     * @param email 用户邮箱
+     */
+    @Override
+    public void resetPassword(String email) {
+
+    }
+
+    /**
+     * 锁定账户
+     *
+     * @param id 账户ID
+     */
+    @Override
+    public void lockAccount(Integer id) {
+
+    }
+
+    /**
+     * 解锁账户
+     *
+     * @param id 账户ID
+     */
+    @Override
+    public void unlockAccount(Integer id) {
+
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public List<Admin> selectAll() {
+        return List.of();
+    }
+
+    /**
+     * 根据主键ID更新实体对象
+     *
+     * @param id 主键ID（Long类型）
+     */
+    @Override
+    public void updateById(Long id) {
+
+    }
+
+    /**
+     * @param entity
+     * @return
+     */
+    @Override
+    public boolean save(Admin entity) {
+        return false;
+    }
+
+    /**
+     * @param entityList
+     * @param batchSize
+     * @return
+     */
+    @Override
+    public boolean saveBatch(Collection<Admin> entityList, int batchSize) {
+        return false;
+    }
+
+    /**
+     * @param entityList
+     * @param batchSize
+     * @return
+     */
+    @Override
+    public boolean saveOrUpdateBatch(Collection<Admin> entityList, int batchSize) {
+        return false;
+    }
+
+    /**
+     * @param entity
+     */
+    @Override
+    public void insert(Admin entity) {
+
+    }
+
+    /**
+     * @param entity
+     * @return
+     */
+    @Override
+    public boolean updateById(Admin entity) {
+        return false;
+    }
+
+    /**
+     * @param entityList
+     * @param batchSize
+     * @return
+     */
+    @Override
+    public boolean updateBatchById(Collection<Admin> entityList, int batchSize) {
+        return false;
+    }
+
+    /**
+     * @param entity
+     * @return
+     */
+    @Override
+    public boolean saveOrUpdate(Admin entity) {
+        return false;
+    }
+
+    /**
+     * @param queryWrapper
+     * @param throwEx
+     * @return
+     */
+    @Override
+    public Admin getOne(Wrapper<Admin> queryWrapper, boolean throwEx) {
+        return null;
+    }
+
+    /**
+     * @param queryWrapper
+     * @return
+     */
+    @Override
+    public Map<String, Object> getMap(Wrapper<Admin> queryWrapper) {
+        return Map.of();
+    }
+
+    /**
+     * @param queryWrapper
+     * @param mapper
+     * @param <V>
+     * @return
+     */
+    @Override
+    public <V> V getObj(Wrapper<Admin> queryWrapper, Function<? super Object, V> mapper) {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public BaseMapper<Admin> getBaseMapper() {
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    public Class<Admin> getEntityClass() {
+        return null;
+    }
+
+    /**
+     * @param integer 主键ID（Integer类型）
+     */
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    /**
+     * 分页查询管理员信息列表
+     *
+     * @param pageNum  页码
+     * @param pageSize 每页大小
+     * @param entity   查询条件对象
+     * @return 分页信息对象
+     */
+    @Override
+    public PageInfo<Admin> selectPage(Integer pageNum, Integer pageSize, Admin entity) {
+        return null;
+    }
 }
