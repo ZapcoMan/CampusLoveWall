77e547b0b66c643c58d63b2e343fcc037458af8a
codervibe <3217277135@qq.com>
2025-09-02 16:47:16 +0800

    feat(UserMapper): 添加用户相关映射方法
    - 新增 findByUsername 方法，用于根据用户名查找用户- 新增 save 方法，用于保存用户信息
    - 新增 updatePassword 方法，用于更新用户密码

--- SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/mapper/UserMapper.java
+++ SpringbootCampusLoveWall/src/main/java/org/campuswall/springbootcampuslovewall/mapper/UserMapper.java
@@ -1,10 +1,30 @@
 package org.campuswall.springbootcampuslovewall.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.campuswall.springbootcampuslovewall.admin.entity.Admin;
 import org.campuswall.springbootcampuslovewall.common.core.mapper.BaseMapperPlus;
+import org.campuswall.springbootcampuslovewall.entity.Account;
 import org.campuswall.springbootcampuslovewall.user.entity.User;
 
 @Mapper
 public interface UserMapper extends BaseMapperPlus<User, Integer> {
+        /**
+     * 根据用户名查找用户
+     *
+     * @param username 用户名
+     * @return 用户对象，如果未找到返回null
+     */
     User findByUsername(String username);
+
+    /**
+     * 保存用户信息
+     *
+     * @param user 用户对象
+     * @return 是否保存成功
+     */
+    @Insert("insert into sys_user(username,password) values(#{username},#{password})")
+    boolean save(User user);
+
+    void updatePassword(User user);
 }
